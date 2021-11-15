fb32a9fc90b4a0ae3f046306c173e41926f13429
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-15 05:47:46 +0530

    Wellcome to address book management System

---
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+public class AddressBookSystemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To AddressBook management System");
+    }
+}
